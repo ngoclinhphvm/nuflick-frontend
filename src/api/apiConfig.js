c1bc91214d720ebe0a441c7962e181608fbce237
novas2061 <22028269@vnu.edu.vn>
2024-04-01 21:27:42 +0700

    refactor: remove unimportant files

--- src/api/apiConfig.js
+++
@@ -1,8 +0,0 @@
-const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'YOUR_API_KEY',
-    backdropImg: (imgPath) => `https://image.tmdb.org/t/p/original${imgPath}`,
-    posterImg: (imgPath) => `https://image.tmdb.org/t/p/w500${imgPath}`
-}
-
-export default apiConfig;

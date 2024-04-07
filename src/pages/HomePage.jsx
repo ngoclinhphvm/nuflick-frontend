28eee2465339c5347e3f72bd65c446f0ca7cafaf
nlphvm <ngoclinhphvm@gmail.com>
2024-04-07 23:52:01 +0700

    refactor: update HomePage to use modified components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from "react";
-import MovieGrid from "../components/common/MovieGrid.jsx";
 import movieAPI from "../api/modules/movie.api.js";
-import personApi from "../api/modules/person.api.js";
-import PersonItem from "../components/common/PersonItem.jsx";
+import MediaGrid from "../components/common/MediaGrid.jsx";
+import Container from "../components/common/Container.jsx";
+
 function HomePage() {
   const [popularMovies, setPopularMovies] = useState([]);
   const [topRatedMovies, setTopRatedMovies] = useState([]);
   const [upcomingMovies, setUpcomingMovies] = useState([]);
-  const [person, setPerson] = useState(null);
 
   useEffect(() => {
     const getPopularMovies = async () => {
       try {
-        const popularMovies = await movieAPI.getMovieList({ movieType: "popular"});
+        const popularMovies = await movieAPI.getMovieList({
+          movieType: "popular",
+        });
         if (popularMovies.response) {
           let top4 = popularMovies.response.data.results.slice(0, 4);
           setPopularMovies(top4);
@@ -26,7 +27,9 @@ function HomePage() {
 
     const getTopRatedMovies = async () => {
       try {
-        const topRatedMovies = await movieAPI.getMovieList({ movieType: "top_rated" });
+        const topRatedMovies = await movieAPI.getMovieList({
+          movieType: "top_rated",
+        });
         if (topRatedMovies.response) {
           let top4 = topRatedMovies.response.data.results.slice(0, 4);
           setTopRatedMovies(top4);
@@ -36,11 +39,13 @@ function HomePage() {
       } catch (error) {
         console.error("Error fetching top rated movies:", error);
       }
-    }
+    };
 
     const getUpcomingMovies = async () => {
       try {
-        const upcomingMovies = await movieAPI.getMovieList({ movieType: "upcoming" });
+        const upcomingMovies = await movieAPI.getMovieList({
+          movieType: "upcoming",
+        });
         if (upcomingMovies.response) {
           let top4 = upcomingMovies.response.data.results.slice(0, 4);
           setUpcomingMovies(top4);
@@ -50,35 +55,23 @@ function HomePage() {
       } catch (error) {
         console.error("Error fetching upcoming movies:", error);
       }
-    }
-    const getPerson = async ()=> {
-      try {
-        const personDetail = await personApi.getDetails(52);
-        if(personDetail) {
-          setPerson(personDetail);
-        } else {
-          console.error("Error fetching person:", personDetail.err);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    }
+    };
     getPopularMovies();
     getTopRatedMovies();
     getUpcomingMovies();
-    getPerson();
   }, []);
 
-  
   return (
     <>
-      <PersonItem person={{
-        name:  person ? person.name : "" ,
-        profile_path: person ? person.profile_path : ""
-      }}/>
-      <MovieGrid movies={popularMovies} moviesType="popular"/>
-      <MovieGrid movies={topRatedMovies} moviesType="top rated"/>
-      <MovieGrid movies={upcomingMovies} moviesType="upcoming"/>
+      <Container header="popular">
+        <MediaGrid medias={popularMovies} mediaType="movie" />
+      </Container>
+      <Container header="top-rated">
+        <MediaGrid medias={topRatedMovies} mediaType="movie" />
+      </Container>
+      <Container header="upcoming">
+        <MediaGrid medias={upcomingMovies} mediaType="movie" />
+      </Container>
     </>
   );
 }

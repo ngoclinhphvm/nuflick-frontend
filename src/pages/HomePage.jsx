d8c65d78beeac18d68915ca50d2dd8bc29b93b94
longsobad <nguyenlongdz2004@gmail.com>
2024-04-12 20:38:20 +0700

    feat: add mostPopularMovie

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,77 +2,156 @@ import React, { useEffect, useState } from "react";
 import movieAPI from "../api/modules/movie.api.js";
 import MediaGrid from "../components/common/MediaGrid.jsx";
 import Container from "../components/common/Container.jsx";
+import uiConfigs from "../configs/ui.configs.js";
+import { Box, Button, Chip, Divider, Stack, Typography } from "@mui/material";
+import ImageHeader from "../components/common/ImageHeader";
+import CircularRate from "../components/common/CircularRate";
+import { Link } from "react-router-dom";
 
 function HomePage() {
-  const [popularMovies, setPopularMovies] = useState([]);
-  const [topRatedMovies, setTopRatedMovies] = useState([]);
-  const [upcomingMovies, setUpcomingMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState(null);
+  const [topRatedMovies, setTopRatedMovies] = useState(null);
+  const [upcomingMovies, setUpcomingMovies] = useState(null);
+  const [mostPopularMovie, setMostPopularMovie] = useState(null);
 
   useEffect(() => {
-    const getPopularMovies = async () => {
+    const fetchData = async () => {
       try {
-        const popularMovies = await movieAPI.getMovieList({
+        const popularMoviesResponse = await movieAPI.getMovieList({
           movieType: "popular",
         });
-        if (popularMovies.response) {
-          let top4 = popularMovies.response.data.results.slice(0, 4);
+        if (popularMoviesResponse.response) {
+          const popularMoviesData = popularMoviesResponse.response.data.results;
+          const top1 = popularMoviesData[0];
+          const top4 = popularMoviesData.slice(1, 5);
+          setMostPopularMovie(top1);
           setPopularMovies(top4);
-        } else if (popularMovies.err) {
-          console.error("Error fetching popular movies:", popularMovies.err);
+        } else if (popularMoviesResponse.err) {
+          console.error(
+            "Error fetching popular movies:",
+            popularMoviesResponse.err
+          );
         }
-      } catch (error) {
-        console.error("Error fetching popular movies:", error);
-      }
-    };
 
-    const getTopRatedMovies = async () => {
-      try {
-        const topRatedMovies = await movieAPI.getMovieList({
+        const topRatedMoviesResponse = await movieAPI.getMovieList({
           movieType: "top_rated",
         });
-        if (topRatedMovies.response) {
-          let top4 = topRatedMovies.response.data.results.slice(0, 4);
+        if (topRatedMoviesResponse.response) {
+          const topRatedMoviesData =
+            topRatedMoviesResponse.response.data.results;
+          const top4 = topRatedMoviesData.slice(0, 4);
           setTopRatedMovies(top4);
-        } else if (topRatedMovies.err) {
-          console.error("Error fetching top rated movies:", topRatedMovies.err);
+        } else if (topRatedMoviesResponse.err) {
+          console.error(
+            "Error fetching top rated movies:",
+            topRatedMoviesResponse.err
+          );
         }
-      } catch (error) {
-        console.error("Error fetching top rated movies:", error);
-      }
-    };
 
-    const getUpcomingMovies = async () => {
-      try {
-        const upcomingMovies = await movieAPI.getMovieList({
+        const upcomingMoviesResponse = await movieAPI.getMovieList({
           movieType: "upcoming",
         });
-        if (upcomingMovies.response) {
-          let top4 = upcomingMovies.response.data.results.slice(0, 4);
+        if (upcomingMoviesResponse.response) {
+          const upcomingMoviesData =
+            upcomingMoviesResponse.response.data.results;
+          const top4 = upcomingMoviesData.slice(0, 4);
           setUpcomingMovies(top4);
-        } else if (upcomingMovies.err) {
-          console.error("Error fetching upcoming movies:", upcomingMovies.err);
+        } else if (upcomingMoviesResponse.err) {
+          console.error(
+            "Error fetching upcoming movies:",
+            upcomingMoviesResponse.err
+          );
         }
       } catch (error) {
-        console.error("Error fetching upcoming movies:", error);
+        console.error("Error fetching movies:", error);
       }
     };
-    getPopularMovies();
-    getTopRatedMovies();
-    getUpcomingMovies();
+
+    fetchData();
   }, []);
 
+  let posterPath = "";
+  let src = "";
+  if (mostPopularMovie) {
+    posterPath = mostPopularMovie.backdrop_path || mostPopularMovie.poster_path;
+    src = posterPath
+      ? `https://image.tmdb.org/t/p/original${posterPath}`
+      : "/no_image.jpg";
+  }
+
   return (
-    <>
-      <Container header="popular">
-        <MediaGrid medias={popularMovies} mediaType="movie" />
-      </Container>
-      <Container header="top-rated">
-        <MediaGrid medias={topRatedMovies} mediaType="movie" />
-      </Container>
-      <Container header="upcoming">
-        <MediaGrid medias={upcomingMovies} mediaType="movie" />
-      </Container>
-    </>
+    <div style={{ width: "99%" }}>
+      {mostPopularMovie && (
+        <>
+          <Link to={`/movie/${mostPopularMovie.id}`}>
+            <ImageHeader imgPath={src} />
+          </Link>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: { md: "row", xs: "column" },
+              marginTop: { xs: "-10rem", md: "-15rem", lg: "-20rem" },
+            }}
+          >
+            <Box
+              sx={{
+                width: { xs: "70%", sm: "40%", md: "20%" },
+                margin: { xs: "0 auto 2rem", md: "0 4rem 0 5rem" },
+              }}
+            >
+              <Box
+                sx={{
+                  paddingTop: "140%",
+                  ...uiConfigs.style.backgroundImage(src),
+                }}
+              />
+            </Box>
+            <Box
+              sx={{
+                width: { xs: "100%", md: "60%" },
+                color: "text.primary",
+              }}
+            >
+              <Typography
+                variant="h4"
+                fontSize={{ xs: "2rem", md: "2rem", lg: "4rem" }}
+                fontWeight="700"
+                sx={{ ...uiConfigs.style.typoLines(2, "left") }}
+              >
+                {`${mostPopularMovie.title || mostPopularMovie.name} ${
+                  mostPopularMovie.release_date.split("-")[0]
+                }`}
+              </Typography>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <CircularRate value={mostPopularMovie.vote_average} />
+                <Divider orientation="vertical" />
+              </Stack>
+              <Typography
+                variant="body1"
+                sx={{ ...uiConfigs.style.typoLines(5) }}
+              >
+                {mostPopularMovie.overview}
+              </Typography>
+            </Box>
+          </Box>
+        </>
+      )}
+      {popularMovies && (
+        <Container header="popular">
+          <MediaGrid medias={popularMovies} mediaType="movie" />
+        </Container>
+      )}
+      {topRatedMovies && (
+        <Container header="top-rated">
+          <MediaGrid medias={topRatedMovies} mediaType="movie" />
+        </Container>
+      )}
+      {upcomingMovies && (
+        <Container header="upcoming">
+          <MediaGrid medias={upcomingMovies} mediaType="movie" />
+        </Container>
+      )}
+    </div>
   );
 }
 

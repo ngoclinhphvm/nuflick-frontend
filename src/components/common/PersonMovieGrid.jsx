89347b28824a8201446ff77b13694488a8277468
longsobad <nguyenlongdz2004@gmail.com>
2024-04-25 16:14:07 +0700

    refactor: change type of button, the number of movies per row

--- src/components/common/PersonMovieGrid.jsx
+++ src/components/common/PersonMovieGrid.jsx
@@ -1,4 +1,4 @@
-import { Button, Grid } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 import { useEffect, useState } from "react";
 import personApi from "../../api/modules/person.api.js";
 import MediaGrid from "./MediaGrid.jsx";
@@ -6,7 +6,7 @@ const PersonMovieGrid = ({ personId, type }) => {
   const [movies, setMovies] = useState([]);
   const [filteredMovies, setFilteredMovies] = useState([]);
   const [page, setPage] = useState(1);
-  const skip = 8;
+  const skip = 12;
 
   useEffect(() => {
     const getMovies = async () => {
@@ -15,17 +15,16 @@ const PersonMovieGrid = ({ personId, type }) => {
         if (response) {
           let MoviesSorted = [];
           if (type === "cast") {
-          MoviesSorted = response.cast.sort(
-            (a, b) => getReleaseDate(b) - getReleaseDate(a)
-          );
+            MoviesSorted = response.cast.sort(
+              (a, b) => getReleaseDate(b) - getReleaseDate(a)
+            );
           } else if (type === "crew") {
-           MoviesSorted = response.crew.sort(
-            (a, b) => getReleaseDate(b) - getReleaseDate(a)
-          );
+            MoviesSorted = response.crew.sort(
+              (a, b) => getReleaseDate(b) - getReleaseDate(a)
+            );
           }
           setMovies([...MoviesSorted]);
           setFilteredMovies([...MoviesSorted].splice(0, skip));
-        
         } else {
           console.error("Error fetching person:", response.err);
         }
@@ -50,12 +49,12 @@ const PersonMovieGrid = ({ personId, type }) => {
   };
 
   return (
-    <>
-      <MediaGrid mediaList={filteredMovies} mediaType="movie"/>
+    <Stack direction="column" alignItems="center" spacing={2}>
+      <MediaGrid mediaList={filteredMovies} mediaType="movie" />
       {filteredMovies.length < movies.length && (
-        <Button onClick={onLoadMore}>load more</Button>
+        <Button variant="outlined" size="medium" onClick={onLoadMore}>Load More</Button>
       )}
-    </>
+    </Stack>
   );
 };
 

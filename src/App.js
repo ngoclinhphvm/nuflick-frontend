0b871d86c1b9daaf93a50d337e02135e9d9ba133
longsobad <nguyenlongdz2004@gmail.com>
2024-04-25 16:25:35 +0700

    refactor: Add an always submerged footer effect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link, Navigate, useNavigate} from "react-router-dom";
-import React, { useState, useEffect } from "react"; // Import useState từ 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 import Login from "./components/Login/index.js";
 import SignUp from "./components/SignUp/index.js";
 import HomePage from "./pages/HomePage.jsx";
@@ -11,39 +11,28 @@ import Discover from "./pages/Discover.jsx";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import MovieSearch from "./pages/MediaSearch.jsx";
 import Footer from "./components/common/Footer/Footer.jsx";
-
-import Review from "./components/Review/Review.jsx";
+import { Box } from "@mui/material";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [username, setUsername] = useState("");
-
-  const handleLogin = (usernameInput) => {
-
-    setUsername(usernameInput);
-  }
-
   return (
-  
     <BrowserRouter>
-    
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/movie/:movieId" element={<MovieDetail />} />
-        <Route path="/movie" element={<Discover/>} />
-        <Route path="/login" element={ <Login />} />
-        <Route path="/signup" element={ <SignUp />}/>
-        <Route path="/account/:username" element={<AccountDetail/>} />
-        <Route path="/person/:personId" element={<PersonDetail />} />
-        <Route path="/search" element={<MovieSearch />} />
-        <Route path="/reviews/:movieId" element={<Review />} />
-      </Routes>
-
-    <Footer></Footer>
-   
+      <Box display="flex" flexDirection="column" minHeight="100vh">
+        <NavBar />
+        <Box flex="1">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movie/:movieId" element={<MovieDetail />} />
+            <Route path="/movie" element={<Discover />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/account/:username" element={<AccountDetail />} />
+            <Route path="/person/:personId" element={<PersonDetail />} />
+            <Route path="/search" element={<MovieSearch />} />
+          </Routes>
+        </Box>
+        <Footer style={{ marginTop: "auto" }} />
+      </Box>
     </BrowserRouter>
-  
   );
 }
 

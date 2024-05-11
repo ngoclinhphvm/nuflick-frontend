4fec84e0af06773a629effb097e6ba35b95dba13
longsobad <nguyenlongdz2004@gmail.com>
2024-05-11 12:32:50 +0700

    refactor: ajust updatePassword display

--- src/pages/UpdatePassword/UpdatePassword.jsx
+++ src/pages/UpdatePassword/UpdatePassword.jsx
@@ -14,20 +14,25 @@ import { IconButton, InputAdornment, OutlinedInput } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { Input } from "@mui/material";
 import { FilledInput } from "@mui/material";
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function UpdatePassword() {
   const { username } = useParams();
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
-  const [formData, setFormData] = useState({ oldPassword: "", newPassword: "", confirmNewPassword: ""});
+  const [formData, setFormData] = useState({
+    oldPassword: "",
+    newPassword: "",
+    confirmNewPassword: "",
+  });
   const [showOldPassword, setShowOldPassword] = React.useState(false);
   const [showNewPassword, setShowNewPassword] = React.useState(false);
   const [showConfirmNewPassword, setShowConfirmNewPassword] = useState(false);
-  
+
   const handleClickShowOldPassword = () => setShowOldPassword((prev) => !prev);
   const handleClickShowNewPassword = () => setShowNewPassword((show) => !show);
-  const handleClickShowConfirmNewPassword = () => setShowConfirmNewPassword((show) => !show);
+  const handleClickShowConfirmNewPassword = () =>
+    setShowConfirmNewPassword((show) => !show);
 
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
@@ -35,18 +40,18 @@ function UpdatePassword() {
   const handleSubmit = (event) => {
     event.preventDefault();
     if (formData.newPassword !== formData.confirmNewPassword) {
-        toast.error("New password and confirm new password do not match");
-        return; 
-      }
+      toast.error("New password and confirm new password do not match");
+      return;
+    }
     if (formData.newPassword.length < 6) {
-        toast.error("Password must be at least 6 characters");
-        return;
+      toast.error("Password must be at least 6 characters");
+      return;
     }
     if (formData.newPassword === formData.oldPassword) {
-        toast.error("New password must be different from old password");
-        return;
+      toast.error("New password must be different from old password");
+      return;
     }
-    
+
     const data = {
       oldPassword: formData.oldPassword,
       newPassword: formData.newPassword,
@@ -54,13 +59,13 @@ function UpdatePassword() {
     accountApi.updatePassword(username, data, token).then((res) => {
       console.log(res);
       if (!res.success) {
-        console.log("res.message",res.message)
+        console.log("res.message", res.message);
         console.log("update failed");
         toast.error(res.message);
       } else {
         console.log("update success");
         toast.success("Update success");
-        navigate('/');
+        navigate("/");
       }
     });
   };
@@ -74,107 +79,116 @@ function UpdatePassword() {
 
   return (
     <>
-    <Box padding="20px" paddingLeft={"50px"} 
-    paddingRight={"50px"} display="flex" justifyContent="left" alignItems="left" sx={{ height: "100vh" }
-  }
-    
-    >
-    <Stack direction="column" spacing={4}>
-    <Container header="Update Password">
-    <form onSubmit={handleSubmit}>
-        <Stack direction="column" spacing={2}>
-            <Typography variant="h6">Old Password</Typography>
-            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showOldPassword ? 'text' : 'password'}
-            name="oldPassword"
-            value={formData.oldPassword}
-            onChange={handleChange}
+      <Box
+        padding="2em"
+        display="flex"
+        justifyContent="left"
+        alignItems="left"
+        sx={{ height: "100vh" }}
+      >
+         <Stack direction="column" spacing={4} width="100%">
+          <Container header="Update Password">
+            <form onSubmit={handleSubmit}>
+              <Stack direction="column" spacing={2} width="100%">
+                <Typography variant="h6">Old Password</Typography>
+                <FormControl sx={{ m: 1, width: "30%" }} variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showOldPassword ? "text" : "password"}
+                    name="oldPassword"
+                    value={formData.oldPassword}
+                    onChange={handleChange}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowOldPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showOldPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                  />
+                </FormControl>
+                <Typography variant="h6">New Password</Typography>
 
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowOldPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
+                <FormControl sx={{ m: 1, width: "30%" }} variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showNewPassword ? "text" : "password"}
+                    name="newPassword"
+                    value={formData.newPassword}
+                    onChange={handleChange}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowNewPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showNewPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                  />
+                </FormControl>
+                <Typography variant="h6">Confirm new password</Typography>
+                <FormControl sx={{ m: 1, width: "30%" }} variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showConfirmNewPassword ? "text" : "password"}
+                    name="confirmNewPassword"
+                    value={formData.confirmNewPassword}
+                    onChange={handleChange}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowConfirmNewPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showConfirmNewPassword ? (
+                            <VisibilityOff />
+                          ) : (
+                            <Visibility />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                  />
+                </FormControl>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  size="large"
+                  sx={{ width: "max-content", backgroundColor: "darkred" }}
+                  startIcon={<SendOutlinedIcon />}
+                  loadingPosition="start"
                 >
-                  {showOldPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-        </FormControl>
-      <Typography variant="h6">New Password</Typography>
-
-      <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showNewPassword ? 'text' : 'password'}
-            name="newPassword"
-            value={formData.newPassword}
-            onChange={handleChange}
-            
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowNewPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showNewPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-        </FormControl>
-         <Typography variant="h6">Confirm new password</Typography>
-         <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showConfirmNewPassword ? 'text' : 'password'}
-            name="confirmNewPassword"
-            value={formData.confirmNewPassword}
-            onChange={handleChange}
-            
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowConfirmNewPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showConfirmNewPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-        </FormControl>
-  <Button
-            type="submit"
-            variant="contained"
-            size="large"
-            sx={{ width: "max-content", backgroundColor: "darkred" }}
-            startIcon={<SendOutlinedIcon />}
-            loadingPosition="start"
-        >
-            Submit
-        </Button>
-    </Stack>
-    </form>
-    </Container>
-    </Stack>
-    </Box>
-    <ToastContainer />
+                  Update
+                </Button>
+              </Stack>
+            </form>
+          </Container>
+        </Stack>
+      </Box>
+      <ToastContainer />
     </>
   );
 }
